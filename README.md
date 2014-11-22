53c2365e287fce04029c1dcd057073e648fe4de6
happyeric1120 <happyeric1120@gmail.com>
2014-11-22 18:28:32 -0500

    Added content in README.md and modified run_analysis.R with the option of using dplyr package

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-GettingAndCleaningDataCourseProject
-===================================
+Getting And Cleaning Data Course Project
+========================================
 
-Getting and cleaning data course project
 The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set. The data is from Human Activity Recognition Using Smartphone Data Set, UCI Machine Learning Repository. (http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
 
 
@@ -10,6 +9,35 @@ This repoitory includes the following files:
 - 'README.md'
 
 - 'run_analysis.R': This script combines all the training and test dataset including the labels. It extracts only the features with mean and std (standard deviation) and the class labels has already been changed to activity name. (The orignal class is labeled as number.)
+
+
+How to run the run_analysis.R:
+==============================
+The run_analysis.R fetches the files from (archive.ics.uci.edu/ml/machine-learning-databases/00240/UCI%20HAR%20Dataset.zip).
+
+This script has to place at the same folder with the data folder where you extract your downloaded file.
+
+Folder setup:
+The default folder name is "UCI HAR Dataset". If you name the folder of dataset with different name, you can change the variable "dataFolderName" at the very first beginning of the script.
+Please do not change any files' names or folders' names within the dataset folder. Please keep the folder structure. The folder structure is described as bellow:
+
+   /UCI HAR Dataset
+       activity_labels.txt
+       features.txt
+       features_info.txt
+       README.txt
+       /train
+           subject_train.txt
+           X_train.txt
+           y_train.txt
+           /Inertial Signals
+       /test
+           subject_test.txt
+           X_test.txt
+           y_test.txt
+           Inertial Signals
+           
+The script will also ask you if the dplyr package is installed in your R. If so, you can answer "yes" and the speed of creating the tidy data will be faster. 
 
 Notes:
 ======
--- run_analysis.R
+++ run_analysis.R
@@ -1,24 +1,27 @@
+##########     General setting before running    ##########
+dataFolderName = "UCI HAR Dataset"
+
 ##########     First part: Read all necessary files     ########## 
 
 # Read the training dataset from /train/
-X_train <- read.table("UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
-subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+X_train <- read.table(paste(dataFolderName, "/train/X_train.txt", sep=""))
+y_train <- read.table(paste(dataFolderName, "/train/y_train.txt", sep=""))
+subject_train <- read.table(paste(dataFolderName, "/train/subject_train.txt", sep=""))
 
 # Read the test dataset from /test/
-X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
-subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+X_test <- read.table(paste(dataFolderName, "/test/X_test.txt", sep=""))
+y_test <- read.table(paste(dataFolderName, "/test/y_test.txt", sep=""))
+subject_test <- read.table(paste(dataFolderName, "/test/subject_test.txt", sep=""))
 
 # Read the Activity name from activity_labels.txt and 
 # convert y_train and y_test to activity names
-activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+activity_labels <- read.table(paste(dataFolderName, "/activity_labels.txt", sep=""))
 y_train_activity_labels <- sapply(y_train, function(x) x <- activity_labels$V2[x])
 y_test_activity_labels <- sapply(y_test, function(x) x <- activity_labels$V2[x])
 
 
 # Read the feature name from features.txt
-features <- read.table("UCI HAR Dataset/features.txt")
+features <- read.table(paste(dataFolderName, "/features.txt", sep=""))
 
 ##########     End of the first part     ########## 
 
@@ -51,39 +54,53 @@ Final_dataset <- cbind(subject_dataset, X_dataset_selected_feature, activity_dat
 
 ##########     Final part: tidy dataset with the avg of     ##########
 ##########     each variable and each subject               ##########
+dplyrInstalled <- readline("Do you have dplyr package installed? (Yes/No): ")
 
-## If the dplyr library is not installed  ##
-activity_raw_dataset <- rbind(y_train, y_test)
-colnames(activity_raw_dataset) <- "activity"
-Final_dataset_backup <- cbind(subject_dataset, activity_raw_dataset, X_dataset_selected_feature)
 
-Final_group_data <- data.frame()
-
-for (i in unique(Final_dataset_backup$subject)) {
-    for (j in unique(Final_dataset_backup$activity)) {
-        subset_data <- subset(Final_dataset_backup, Final_dataset_backup$subject == i &
-                              Final_dataset_backup$activity == j)
-        df_colmean <- colMeans(subset_data)
-        Final_group_data <- rbind(Final_group_data, df_colmean)
-    } 
+if (grepl("[Nn][Oo]", dplyrInstalled)) {
+    
+    ## If the dplyr library is not installed  ##
+    
+    activity_raw_dataset <- rbind(y_train, y_test)
+    colnames(activity_raw_dataset) <- "activity"
+    Final_dataset_backup <- cbind(subject_dataset, activity_raw_dataset, X_dataset_selected_feature)
+    
+    Final_group_data <- data.frame()
+    
+    for (i in unique(Final_dataset_backup$subject)) {
+        for (j in unique(Final_dataset_backup$activity)) {
+            subset_data <- subset(Final_dataset_backup, Final_dataset_backup$subject == i &
+                                      Final_dataset_backup$activity == j)
+            df_colmean <- colMeans(subset_data)
+            Final_group_data <- rbind(Final_group_data, df_colmean)
+        } 
+    }
+    colnames(Final_group_data) <- c("subject", "activity", as.character(names_selected_feature))
+    Final_group_data$activity <- sapply(Final_group_data$activity, function(x) x <- activity_labels$V2[x])
+    Final_group_data <- Final_group_data[order(Final_group_data$subject, Final_group_data$activity),]
+    # write.csv(Final_group_data, file = "resNoLib.csv")
+    write.table(Final_group_data, file = "tidyDataset.txt", row.names = FALSE)
+    
+    message("New tidy dataset created! (tidyDataset.txt)")
+    
+} else if (grepl("[Yy][Ee][Ss]", dplyrInstalled)) {
+    
+    ## If the dplyr library is not installed  ##
+    
+    library(dplyr)
+    test_dataset <- Final_dataset
+    test_dataset_tbl <- tbl_df(test_dataset)
+    test_dataset_tbl_grouped <- group_by(test_dataset_tbl, subject, activity)
+    res <- summarise_each(test_dataset_tbl_grouped, funs(mean))
+    # write.csv(res, file="result.csv")
+    write.table(res, file = "tidyDataset.txt", row.names = FALSE)
+    
+    message("New tidy dataset created! (tidyDataset.txt)")
+    
+#     res1 <- test_dataset_tbl %>%
+#         group_by(subject, activity) %>%
+#         summarise_each(funs(mean))
+    
+} else {
+    message("Can't read your answer! Please run the script again!")
 }
-colnames(Final_group_data) <- c("subject", "activity", as.character(names_selected_feature))
-Final_group_data$activity <- sapply(Final_group_data$activity, function(x) x <- activity_labels$V2[x])
-Final_group_data <- Final_group_data[order(Final_group_data$subject, Final_group_data$activity),]
-# write.csv(Final_group_data, file = "resNoLib.csv")
-write.table(res, file = "resultNoLib.txt", row.names = FALSE)
-
-
-
-## If the dplyr library is installed  ##
-library(dplyr)
-test_dataset <- Final_dataset
-test_dataset_tbl <- tbl_df(test_dataset)
-test_dataset_tbl_grouped <- group_by(test_dataset_tbl, subject, activity)
-res <- summarise_each(test_dataset_tbl_grouped, funs(mean))
-# write.csv(res, file="result.csv")
-write.table(res, file = "result.txt", row.names = FALSE)
-
-res1 <- test_dataset_tbl %>%
-    group_by(subject, activity) %>%
-    summarise_each(funs(mean))
